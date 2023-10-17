44b3fa3a99bd6356dd2436f1f606c40568b04c7a
Findus <github@f1ndus.de>
2023-10-17 12:54:35 +0200

    bznz

---
+++ userscript.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         🅱znz
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+     function replace(element) {
+        Array.from(element.childNodes.entries(), ([key, value]) => value).map(element => {
+                    replace(element);
+        });
+        if (element.firstChild !== null && element.firstChild.nodeType === Node.TEXT_NODE && element.tagName !== "STYLE" ) {
+            element.firstChild.textContent = element.firstChild.textContent.replace(/business/gi, 'BZNZ');
+        }
+    }
+
+
+    // Your code here...
+    // Select the node that will be observed for mutations
+    const targetNode = document.body;
+
+    replace(targetNode);
+
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: true, childList: true, subtree: true };
+
+
+    // Callback function to execute when mutations are observed
+    const callback = (mutationList, observer) => {
+        for (const mutation of mutationList) {
+            if (mutation.type === "childList") {
+               // console.log("A child node has been added or removed.");
+                //mutation.addedNodes.map(e => console.log("node"));
+                Array.from(mutation.addedNodes.entries(), ([key, value]) => value).map(element => {
+                    replace(element);
+
+                });
+            } else if (mutation.type === "attributes") {
+               // console.log(`The ${mutation.attributeName} attribute was modified.`);
+            }
+        }
+    };
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
+
+})();
